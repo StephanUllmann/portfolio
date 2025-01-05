d2314f3fc07c37fbb008aaa298f8b41bd25ec473
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-01-05 19:01:33 +0100

    refactor: update prefetch strategy and improve ProjectCard styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import expressiveCode from "astro-expressive-code";
-
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  // markdown: {
-  //   shikiConfig: {
-  //     // Choose from Shiki's built-in themes (or add your own)
-  //     // https://shiki.style/themes
-  //     // theme: 'everforest-dark',
-  //     theme: 'night-owl',
-  //     langs: ['go', 'javascript', 'csharp', 'html', 'css'],
-  //     wrap: true,
-  //     // Add custom transformers: https://shiki.style/guide/transformers
-  //     // Find common transformers: https://shiki.style/packages/transformers
-  //     // transformers: [],
-  //   },
-  // },
-
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
   integrations: [
     expressiveCode({
       themes: ["monokai"],
